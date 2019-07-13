74e5c1efe2b34517cfef665c54e0936823a9c1fe
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-07-13 19:12:43 +0300

    CRUD Customer

--- LibraryManagement/LibraryManagement/Controllers/CustomerController.cs
+++ LibraryManagement/LibraryManagement/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿
 using LibraryManagement.Data.Interfaces;
+using LibraryManagement.Data.Model;
 using LibraryManagement.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -42,6 +43,44 @@ namespace LibraryManagement.Controllers
             }
 
             return View(customerVM);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var customer = _customerRepository.GetById(id);
+
+            _customerRepository.Delete(customer);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Customer customer)
+        {
+            _customerRepository.Create(customer);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Update(int id)
+        {
+            var customer = _customerRepository.GetById(id);
+            return View(customer);
+        }
+
+        [HttpPost]
+
+        public IActionResult Update(Customer customer)
+        {
+            _customerRepository.Update(customer);
+
+            return RedirectToAction("List");
         }
     }
 }
