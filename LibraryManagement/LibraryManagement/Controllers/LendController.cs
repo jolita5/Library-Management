633ca7cd1e9c0782d6778b7cce0dd9a08b777f1a
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-07-15 21:40:23 +0300

    LendController and Views

---
+++ LibraryManagement/LibraryManagement/Controllers/LendController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using LibraryManagement.Data.Interfaces;
+using LibraryManagement.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LibraryManagement.Controllers
+{
+    public class LendController : Controller
+    {
+
+        private readonly IBookRepository _bookRepository;
+        private readonly ICustomerRepository _customerRepository;
+
+        public LendController(IBookRepository bookRepository, ICustomerRepository customerRepository)
+        {
+            _bookRepository = bookRepository;
+            _customerRepository = customerRepository;
+        }
+
+        [Route("Lend")]
+        public IActionResult List()
+        {
+            var availableBooks = _bookRepository.FindWithAuthor(x => x.BorrowerId == 0);
+
+            if(availableBooks.Count() == 0)
+            {
+                return View("Empty");
+            }
+            else
+            {
+                return View(availableBooks);
+            }
+        }
+
+
+        public IActionResult LendBook(int bookId)
+        {
+            var lendVM = new LendViewModel()
+            {
+                Book = _bookRepository.GetById(bookId),
+                Customers = _customerRepository.GetAll()
+            };
+
+            return View(lendVM);
+        }
+
+        [HttpPost]
+        public IActionResult LendBook(LendViewModel lendViewModel)
+        {
+            var book = _bookRepository.GetById(lendViewModel.Book.BookId);
+            var customer = _customerRepository.GetById(lendViewModel.Book.BorrowerId);
+
+            book.Borrower = customer;
+
+            _bookRepository.Update(book);
+
+            return RedirectToAction("List");
+        }
+    }
+}
