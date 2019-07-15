da2d7349156998f55c5c6da40dd3bba7cc1ceb18
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-07-15 21:57:31 +0300

    Return controller and Views

---
+++ LibraryManagement/LibraryManagement/Controllers/ReturnController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagement.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LibraryManagement.Controllers
+{
+    public class ReturnController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ICustomerRepository _customerRepository;
+
+        public ReturnController(IBookRepository bookRepository, ICustomerRepository customerRepository)
+        {
+            _bookRepository = bookRepository;
+            _customerRepository = customerRepository;
+        }
+
+        [Route("Return")]
+        public IActionResult List()
+        {
+            var borrowedBooks = _bookRepository.FindWithAuthorAndBorrower(x => x.BorrowerId != 0);
+
+            if (borrowedBooks == null || borrowedBooks.ToList().Count() == 0)
+            {
+                return View("Empty");
+            }
+
+            return View(borrowedBooks);
+        }
+
+     
+        public IActionResult ReturnBook(int bookId)
+        {
+            var book = _bookRepository.GetById(bookId);
+
+            book.Borrower = null;
+            book.BorrowerId = 0;
+
+            _bookRepository.Update(book);
+
+            return RedirectToAction("List");
+        }
+    }
+}
