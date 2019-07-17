de9a7577a25655e28be9b40f6be41a44b1e1f004
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-07-17 19:12:13 +0300

    Book model validation

--- LibraryManagement/LibraryManagement/Data/Model/Book.cs
+++ LibraryManagement/LibraryManagement/Data/Model/Book.cs
@@ -1,10 +1,16 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace LibraryManagement.Data.Model
 {
     public class Book
     {
         public int BookId { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
         public string Title { get; set; }
 
         public virtual Author Author { get; set; }
