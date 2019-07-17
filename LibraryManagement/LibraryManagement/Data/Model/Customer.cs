feff1aa5cc15ef6d5a0785df0df937a6f0e98114
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-07-17 19:10:53 +0300

    Data model validation

--- LibraryManagement/LibraryManagement/Data/Model/Customer.cs
+++ LibraryManagement/LibraryManagement/Data/Model/Customer.cs
@@ -1,10 +1,16 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace LibraryManagement.Data.Model
 {
     public class Customer
     {
         public int CustomerId { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
         public string Name { get; set; }
     }
 }
